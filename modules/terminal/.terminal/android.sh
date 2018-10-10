ab1d15db3b20e12343f57cff16873275267c06c0
Dhyego Calota <dhyegofernando@gmail.com>
2018-10-10 13:53:12 -0400

    Update android.sh

--- modules/terminal/.terminal/android.sh
+++ modules/terminal/.terminal/android.sh
@@ -1,4 +1,4 @@
-export ANDROID_SDK_ROOT="`brew --prefix`/share/android-sdk"
+export ANDROID_HOME="`brew --prefix`/share/android-sdk"
 export PATH=$ANDROID_HOME/tools:$PATH
 export PATH=$ANDROID_HOME/platform-tools:$PATH
-export PATH=$ANDROID_HOME/build-tools/26.1.1:$PATH
+export PATH=$ANDROID_HOME/build-tools/28.0.3:$PATH
