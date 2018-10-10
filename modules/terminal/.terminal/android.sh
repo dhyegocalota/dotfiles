8b3d7d00120b1daa8310075448cb82ea87ba4069
Dhyego Calota <dhyegofernando@gmail.com>
2018-10-10 13:37:28 -0400

    Create android.sh

---
+++ modules/terminal/.terminal/android.sh
@@ -0,0 +1,4 @@
+export ANDROID_SDK_ROOT="`brew --prefix`/share/android-sdk"
+export PATH=$ANDROID_HOME/tools:$PATH
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+export PATH=$ANDROID_HOME/build-tools/26.1.1:$PATH
